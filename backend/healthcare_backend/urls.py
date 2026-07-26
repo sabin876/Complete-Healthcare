6c1061e12a445e5b3b431fddc4caea0593b045d1
sabin876 <sabinsabin876@gmail.com>
2026-07-26 20:34:11 +0545

    Redirect root URL / to /admin/

--- backend/healthcare_backend/urls.py
+++ backend/healthcare_backend/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('admin:index'), name='root_redirect'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
