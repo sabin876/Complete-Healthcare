1bf95cc231396cc5dd0414d37865d935dee9d59a
prajwal <prajwalsiwakoti39@gmail.com>
2026-08-21 14:34:59 +0545

    fix: update email settings to use api.corx.ae with SSL configuration

--- backend/healthcare_backend/settings.py
+++ backend/healthcare_backend/settings.py
@@ -242,18 +242,22 @@ JAZZMIN_UI_TWEAKS = {
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() in ('true', '1', 't')
+
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'api.corx.ae')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '465'))
+
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_TIMEOUT = 30
 
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 CONTACT_EMAIL = os.getenv('CONTACT_EMAIL')
+
 
 # Allow large blog article content to be saved without 500 errors
 # Default is 2.5 MB which is too small for rich HTML article content
